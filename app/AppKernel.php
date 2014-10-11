c57482424f00ca14c9fe0d3323d5a9f1c287ec6d
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-10-11 11:04:32 -0400

    Installing stof-doctrine-extensions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PengarWin\PengarWinBundle\PengarWinPengarWinBundle(),
+            new PengarWin\DoubleEntryBundle\PengarWinDoubleEntryBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
