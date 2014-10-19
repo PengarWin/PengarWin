bb357fb6641c99aff84f527d0a9b2e2fbec7ba35
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-10-18 21:22:20 -0400

    Configuring FOSUser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new PengarWin\DoubleEntryBundle\PengarWinDoubleEntryBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
